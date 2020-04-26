436b9e5ff5e9f1e597372033d96b02ececcb06ed
Clement Franch <glukor@hotmail.fr>
2020-04-26 22:31:24 +0200

    Fetching user inputs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 // autobind decorator
+// _ and _2 are not used, hence their special names to hint their non-use to typescript
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
@@ -38,10 +39,42 @@ class ProjectInput {
         this.attach();
     }
 
+    // getting all user input from the form.
+    // expecting it to be a tuple or void if user input is invalid
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        // validation : checking if inputs are not empty
+        if (
+            enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Invalid input, please try again');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+
+    // clear user inputs after form validation
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
 
     private configure() {
