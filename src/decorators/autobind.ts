5eab2638d99b4f08e5f65cfa1de048bdbd9a57f2
Clement Franch <glukor@hotmail.fr>
2020-05-10 21:17:06 +0200

    Splitting the code into modules

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+// autobind decorator
+// _ and _2 are not used, hence their special names to hint their non-use to typescript
+export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
